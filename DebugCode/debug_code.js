1fea571ecd460c7b511266e55a2fbbb78aed6c79
javieralvarezgrafico <javier.alvarez.grafico@gmail.com>
2025-03-25 13:08:17 -0400

    Tarea practica Laboratorio practico debugging en Chrome Dev Tools

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -4,25 +4,45 @@ function performOperation() {
     let num2 = parseInt(document.getElementById('input2').value);
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
-    // Perform the operation
-    let result = multiply(num1, num2);
-        // Display the result
-        displayResult(result);
+        // Perform the operation
+        let result = operation(num1, num2);
+
+        // TAREA PRÁCTICA
+        // Asignamos los valores manualmente a tres variables
+        const multiplication = result[0]; // Multiplicación
+        const sum = result[1]; // Suma
+        const division = result[2]; // División
+        // Mostrar los resultados
+        displayResult(multiplication, sum, division);
+
     } else {
         displayResult('Please enter valid numbers');
     }
 }
 
-function multiply(a, b) {
+/* TAREA DE PRÁCTICA:
+Necesitas realizar operaciones aritméticas como suma,
+multiplicación y división simultáneamente utilizando la misma función.
+
+Además, necesitas verificar el flujo del código,
+que dependerá de la operación aritmética que estés realizando primero.
+
+También, intenta asignar un valor en forma de caracteres
+y observa cómo se muestra este valor utilizando el depurador.*/
+
+function operation(a, b) {
     // Introduce a debugger statement to pause execution
     debugger;
-    // Multiply the numbers
-    return a * b;
+    // TAREA DE PRÁCTICA
+    // Evitar división por cero
+    let divisionResult = (b !== 0) ? a / b : 'Undefined (division by zero)';
+    return [a * b, a + b, divisionResult];
 }
 
-function displayResult(result) {
+function displayResult(result1, result2, result3) {
     // Display the result in the paragraph element
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+    // TAREA DE PRÁCTICA
+    resultElement.textContent = `The result is: Multiplication: ${result1}, Sum: ${result2}, Division: ${result3}, `;
 }
         
